3a7e22316bdcc10c6bb737ee6b6bfaaecaea5e98
svolokitin <volokitin14sv@gmail.com>
2022-09-12 21:35:11 +0300

    add some JavaScript code

---
+++ lab1/myScript.js
@@ -0,0 +1,15 @@
+function myFunctionInner() {
+    document.getElementById('inner').innerHTML = "Hello pidar"
+}
+
+function myFunctionStyle() {
+    document.getElementById('style').style.fontSize = '35px'
+}
+
+function myFunctionOff() {
+    document.getElementById('img').src='pic_bulboff.gif'
+}
+
+function myFunctionOn() {
+    document.getElementById('img').src='pic_bulbon.gif'
+}
